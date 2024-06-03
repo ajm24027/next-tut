152b104ac432c5844a3e72df1a7780fb7f70605e
Anthony Medina <anthony.john.medina@gmail.com>
2024-06-02 20:59:08 -0400

    Chapter 11, added client side URL updates

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -16,6 +16,7 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* The Suspense component receives two props, children and fallback. It then renders fallback until all the data required by children is available and children can be rendered. */}
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
@@ -31,3 +32,11 @@ export default async function Page() {
     </main>
   );
 }
+
+// How I've optimized data fetching in my app:
+// Created a database in the same region as your application code to reduce latency between your server and database.
+// Fetched data on the server with React Server Components. This allows you to keep expensive data fetches and logic on the server, reduces the client-side JavaScript bundle, and prevents your database secrets from being exposed to the client.
+// Used SQL to only fetch the data you needed, reducing the amount of data transferred for each request and the amount of JavaScript needed to transform the data in-memory.
+// Parallelize data fetching with JavaScript - where it made sense to do so.
+// Implemented Streaming to prevent slow data requests from blocking your whole page, and to allow the user to start interacting with the UI without waiting for everything to load.
+// Move data fetching down to the components that need it, thus isolating which parts of your routes should be dynamic in preparation for Partial Prerendering.
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,33 @@
 'use client';
 
+// URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters. Instead of creating a complex string literal, you can use it to get the params string like ?page=1&query=a.
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  // 2. Initialize Search params
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  // 1. Capture the user input > we use the handleSearch to take in a string.
+  // 3. Create an instance of searchParams based on the term coming from the handleSearch Function.
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    // set - what looks to be a method, updates the params based on user input. If it's empty, use delete method to delete the 'query'.
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+    // /pathname/?params=term
+    // /dashboard/invoices?query=lee
+    // Pathname is the usePathname() which sense our current path or /dashboard/invoices.
+    // When we go params.set() we're creating the params that follow that path. Or in our case ('query', term)
+    // The params is query and the query is = term.
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +36,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        // pass an anonymous function taking in the event object or (e), and passing that events.target.value to the handleSearch function.
+        onChange={(e) => handleSearch(e.target.value)}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
