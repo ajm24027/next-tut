cff054f1c23dc58111117997554888c8322bfe88
Anthony Medina <anthony.john.medina@gmail.com>
2024-06-19 11:35:26 -0400

    redid notes for better understanding of implementing useSerachParams,usePathname, and useRouter hooks.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,28 +6,25 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  // 2. Initialize Search params
+  // useSearchParams() - Allows you to access the parameters of the current URL. For example, the search params for this URL /dashboard/invoices?page=1&query=pending would look like this: {page: '1', query: 'pending'}.
   const searchParams = useSearchParams();
+  // usePathName() - Lets you read the current URL's pathname. For example, for the route /dashboard/invoices, usePathname would return '/dashboard/invoices'.
   const pathname = usePathname();
+  // useRouter() - Enables navigation between routes within client components programmatically. We're destructuring or selecting the replace method from that hook. replace - Perform's a client-side navigation to the provided route without adding a new entry into the browser’s history stack.
   const { replace } = useRouter();
 
-  // 1. Capture the user input > we use the handleSearch to take in a string.
-  // 3. Create an instance of searchParams based on the term coming from the handleSearch Function.
+  // 1. Initializing a handleSearch function, that's using useDebouncedCallback(), the debounce will allow us to wait up to 300ms before calling the function back. This will let us wait 3s to collect input from the user, that way we don't fire off the function on everytime term updates per keystroke.
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
+    // 2. Initialize a params variable that is a new URLSearchParams object.
     const params = new URLSearchParams(searchParams);
-    // set - what looks to be a method, updates the params based on user input. If it's empty, use delete method to delete the 'query'.
+    // If term is present, SET the search param of Query to whatever term is. If there is no term (which is how it should be at the start), delete 'query' from the search params in the URL.
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-    // /pathname/?params=term
-    // /dashboard/invoices?query=lee
-    // Pathname is the usePathname() which sense our current path or /dashboard/invoices.
-    // When we go params.set() we're creating the params that follow that path. Or in our case ('query', term)
-    // The params is query and the query is = term.
   }, 300);
 
   return (
@@ -38,8 +35,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        // pass an anonymous function taking in the event object or (e), and passing that events.target.value to the handleSearch function.
+        // pass an anonymous function taking in the event object or (e), and passing that events.target.value to the handleSearch function - in the handleSearch, this e.target.value is passed as term.
         onChange={(e) => handleSearch(e.target.value)}
+        // default value is going to be populated by whatever the current search param is of query ("?query="). For example, when I manually type in ?query=test in the URL and press enter. The defualt value of this input is turned into test.
         defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
