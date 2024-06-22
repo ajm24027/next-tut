c0fd0cc7e9c4d50840f8f5da63834e6cbd05a7b1
Anthony Medina <anthony.john.medina@gmail.com>
2024-06-22 09:23:11 -0400

    added form validation, invoice creation, invoice creation action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,10 @@
 'use server';
 
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
 // Tip: If you're working with forms that have many fields, you may want to consider using the entries()
 // method with JavaScript's Object.fromEntries()
 
@@ -7,12 +12,30 @@
 
 // const rawFormData = Object.fromEntries(formData.entries())
 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  // console.log({ customerId, amountInCents, status, date }, typeof amount);
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+`;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
